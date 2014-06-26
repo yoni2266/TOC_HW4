f6fa5862ff925b1ba7fc240793faeb2756e3632e
Ting-Yu Lin <yoni2266@hotmail.com>
2014-06-26 11:37:02 +0800

    TOC_hw4.php
    1. same as TOC_hw3.php

---
+++ TOC_hw4.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<meta 
+	http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+<?php 
+	echo "<form name=\"form\" method=\"post\" action=\"hw4.php\">"; 
+	echo "URL : <input type=\"text\" name=\"url\" /> <br>";
+	echo "<input type=\"submit\" name=\"button\" value=\"GO!\" />"; 
+	echo "</form>"; 
+?>
+</body>
+</html>
